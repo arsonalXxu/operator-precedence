c46152ed72363adf92d60a0a5eee13e5a7a64ba7
arsonalXxu <897386402@qq.com>
2020-03-14 11:42:14 +0800

    代码优化

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,16 +1,21 @@
 package com.github.hcsp.calculation;
 
+import java.util.ArrayList;
+
 public class Main {
     // 连接两个字符串，如果字符串为空指针null，则将其当作空字符串处理
     // 现在的实现有明显的bug，请修复之。
     public static String concatString(String a, String b) {
-        if (a == null) {
-            a = "";
-        }
-        if (b == null) {
-            b = "";
-        }
-        return a+b;
+//        if (a == null) {
+//            a = "";
+//        }
+//        if (b == null) {
+//            b = "";
+//        }
+//        return a+b;
+        ArrayList<String> arrayList = new ArrayList<>();
+        return a == null ? "" + (b == null ? "" : b): a + (b == null ? "" : b);
+
     }
 
     public static void main(String[] args) {
